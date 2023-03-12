2e857650616eb73343d7c06bd25ff3afdce50215
sanjeetsk <96577772+sanjeetsk@users.noreply.github.com>
2023-03-12 10:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const inp = document.getElementById("evaluatedText");
+count count=document.getElementById("wordCount");
+inp.addEventListener('input', calculatedWords());
+
+function calculatedWords(e) {
+	e.target.value.split(" ").length;	
+}
